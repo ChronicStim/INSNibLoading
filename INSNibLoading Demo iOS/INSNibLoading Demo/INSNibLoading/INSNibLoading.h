b09087befce114f78c6420a8e3c3bf09bc653131
ChronicStim <support@chronicstimulation.com>
2019-12-28 12:08:57 -0500

    chore: Added Framework target to project & example

---
+++ INSNibLoading Demo iOS/INSNibLoading Demo/INSNibLoading/INSNibLoading.h
@@ -0,0 +1,19 @@
+//
+//  INSNibLoading.h
+//  INSNibLoading
+//
+//  Created by Bob Kutschke on 12/28/19.
+//  Copyright © 2019 Inspace Labs Sp z o. o. Spółka Komandytowa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for INSNibLoading.
+FOUNDATION_EXPORT double INSNibLoadingVersionNumber;
+
+//! Project version string for INSNibLoading.
+FOUNDATION_EXPORT const unsigned char INSNibLoadingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INSNibLoading/PublicHeader.h>
+
+#import "UIView+INSNibLoading.h"
